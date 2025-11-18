1e5b77c9b2865430c5e789b568689f4c03caabcb
Jake Wright <wrightjake5683@gmail.com>
2025-11-18 20:27:13 +0000

    updated code to adjust for laserscan sensor position difference between real robot and simulation

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/qos.hpp"
 #include <algorithm>
 #include <chrono>
+#include <ranges>
 
 class PatrolNode : public rclcpp::Node
 {
@@ -24,7 +25,7 @@ public:
         cmd_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
         auto timer_period = std::chrono::milliseconds(100); //Equivalent to 10Hz
-        //timer_ = this->create_wall_timer(timer_period, std::bind(&PatrolNode::timer_control_callback, this));
+        timer_ = this->create_wall_timer(timer_period, std::bind(&PatrolNode::timer_control_callback, this));
 
         RCLCPP_INFO(this->get_logger(), "%s Ready...", node_name_.c_str());
     }
@@ -48,20 +49,53 @@ private:
 
     void laserscan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
     {
-        auto min_distance = std::min_element(msg->ranges.begin(), msg->ranges.end());
+        /*
+        float min_range = msg->ranges[50];
+        int min_index = 50;
+
+        // Find the index of the minimum range in the laser scan data
+        for (int i = 49 i > -151; i--) {
+            if (i < 0) {i=(-1) *}
+            if (msg->ranges[i] < min_range) {
+                min_range = msg->ranges[i];
+                min_index = i;
+            }
+        }
+
+        RCLCPP_INFO(this->get_logger(), "min_range = %.2f", min_range);
+        RCLCPP_INFO(this->get_logger(), "min_index = %i", min_index);
+
+         if (min_range < .35)
+        {
+            direction_ = msg->angle_min + (min_index * msg->angle_increment);
+            RCLCPP_INFO(this->get_logger(), "direction_ = %.2f", direction_);
+
+        }
+        else
+        {
+            direction_ = 0.0;
+        }
+        */
+
+        int end = 150;
+
+
+        auto min_distance = std::min_element(msg->ranges.begin(), msg->ranges.begin() + end + 1);
         int min_distance_index = std::distance(msg->ranges.begin(), min_distance);
         RCLCPP_INFO(this->get_logger(), "min_distance = %.2f", *min_distance);
         RCLCPP_INFO(this->get_logger(), "min_distance_index = %i", min_distance_index);
 
-
+        /*
         RCLCPP_INFO(this->get_logger(), "100th index = %.2f", msg->ranges[100]);
         RCLCPP_INFO(this->get_logger(), "0 index = %.2f", msg->ranges[0]);
+        RCLCPP_INFO(this->get_logger(), "150 index = %.2f", msg->ranges[150]);
+        RCLCPP_INFO(this->get_logger(), "50 index = %.2f", msg->ranges[50]);
 
 
         RCLCPP_INFO(this->get_logger(), "angle_max = %.2f", msg->angle_max);
         RCLCPP_INFO(this->get_logger(), "angle_min = %.2f", msg->angle_min);
-
-        if ((min_distance_index >= 50 && min_distance_index <= 150) && *min_distance < .35)
+*/
+        if ((min_distance_index < 151) && *min_distance < .35)
         {
             direction_ = msg->angle_min + (min_distance_index * msg->angle_increment);
             RCLCPP_INFO(this->get_logger(), "direction_ = %.2f", direction_);
@@ -71,11 +105,12 @@ private:
         {
             direction_ = 0.0;
         }
+        
     }
 
     void timer_control_callback()
     {
-        double angular = -2 * direction_;
+        double angular = 3.5 * direction_;
         angular = std::max(-1.2, std::min(1.2, angular));
 
         auto msg = geometry_msgs::msg::Twist();
