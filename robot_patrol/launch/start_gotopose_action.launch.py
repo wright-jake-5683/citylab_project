f4d5ca3ec30c3c12486e287f0902fe0afa1050af
Jake Wright <wrightjake5683@gmail.com>
2026-03-09 22:01:11 +0000

    added rviz to gotopose launch

--- robot_patrol/launch/start_gotopose_action.launch.py
+++ robot_patrol/launch/start_gotopose_action.launch.py
@@ -1,10 +1,32 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    # Get Package Description and Directory #
+    package_description = "robot_patrol"
+    package_directory = get_package_share_directory(package_description)
+
+    # Load RViz Configuration File #
+    rviz_config_file = "config.rviz"
+    rviz_config_path = os.path.join(package_directory, 'rviz', rviz_config_file)
+    print("RViz Config Loaded")
+
+    # RViz2 Launch Configuration (RViz) #
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_path],
+    )
+
     return LaunchDescription([
+        rviz_node,
         Node(
             package='robot_patrol',
             executable='go_to_pose_action',
             output='screen'),
-    ])
